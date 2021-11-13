cda69ded77f95c79056d08b37937797852f6b9b4
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2021-11-13 11:40:57 -0800

    End of task 2: React Nucampsite Week 2 Workshop Assignment.   Added card component to the CampsiteInfo Component (to view more info about clicked campsite)

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
 
+    renderCampsite(campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={campsite.image} alt={campsite.name} />
+                            <CardBody>
+                                <CardTitle>
+                                    {campsite.name}
+                                </CardTitle>
+                                <CardText>
+                                    {campsite.description}
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
     renderCampsiteInfo(campsite) {
         if (campsite) {
             return (
             <div className="container">
                 <div className="row">
-
+                    {this.renderCampsite(this.props.campsite)}
                 </div>
             </div>
             );
