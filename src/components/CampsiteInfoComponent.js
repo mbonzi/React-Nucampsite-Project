d14a0c3f6c138a02f67b5b4fbe89d430ab1178d4
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2021-11-30 21:21:28 -0800

    Redux Thunk: Added Reux middleware such as Redux Logger using applyMiddleware() function as argument in Redux's creatStore() function. Then used Redux Thunk to return a function instead of an action from an actread creator, and updated app to support loading and failed views when loading campsite data.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -13,6 +13,8 @@ import {
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import { Loading } from './LoadingComponent';
+
 
 const minLength = (len) => (val) => val && val.length >= len;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -169,7 +171,27 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
-  if (props.campsite) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    if (props.campsite) {
     return (
       <div className="container">
         <div className="row">
