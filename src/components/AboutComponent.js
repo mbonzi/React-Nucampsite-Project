90df9d5565146997b087c37f086a433daf1a57c6
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2021-11-30 20:22:55 -0800

    update

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
 function RenderPartner({partner}) {
     if(partner) {
         return(
